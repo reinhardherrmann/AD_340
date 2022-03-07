757e429a953a229bdb1a51c63245cf6ea0688ccc
RHerrmann <reinhard-ulrich.herrmann@web.de>
2022-03-07 14:53:51 +0100

    step 2  create first app         first Activity designed and tested, theme changed clickListener implemented         very simple validation implemented

--- app/Beschreibung.md
+++ app/Beschreibung.md
@@ -1,1 +1,9 @@
+Quelle Lektion Woche 1
 Quelle Lektion Woche 2  https://www.youtube.com/watch?v=RS7P9vBRonU
+Quelle Lektion Woche 3  https://www.youtube.com/watch?v=7QDZ4NTB9JM
+Quelle Lektion Woche 4
+Quelle Lektion Woche 5
+Quelle Lektion Woche 6
+Quelle Lektion Woche 7
+Quelle Lektion Woche 8
+Quelle Lektion Woche 9
--- app/src/main/java/de/orome/ad340/MainActivity.kt
+++ app/src/main/java/de/orome/ad340/MainActivity.kt
@@ -2,10 +2,54 @@ package de.orome.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        var et_ZipCode: EditText = findViewById(R.id.et_PLZ)
+        val btnEnter: Button = findViewById(R.id.btn_submit_zip_code)
+        var zipCode:String = ""
+
+        btnEnter.setOnClickListener{
+            zipCode = et_ZipCode.text.toString()
+            if (zipCode.length < 5){
+                Toast.makeText(this, getString(R.string.err_msg_zip_code), Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText(this,"PLZ: ${et_ZipCode.text.toString()}",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("MyTag", "onStart aufgerufen")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("MyTag", "onResume aufgerufen")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("MyTag", "onPause aufgerufen")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("MyTag", "onStop aufgerufen")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("MyTag", "onDestroy aufgerufen")
+    }
+
 }
